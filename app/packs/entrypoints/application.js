db0b28416adab9016ca6cd640e199775a60f57f6
Dwight Watson <dwightwatson@users.noreply.github.com>
2021-09-01 21:17:08 +1000

    Set up reproduction

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -2,3 +2,5 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+import "src";
